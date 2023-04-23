c422e3f1f211bef30025a793527b01bf554fc2d8
Nazhmutdin <gumuevnazhmutdin1248@gmail.com>
2023-04-23 07:55:19 +0300

    Fourth commit

--- keyboard.py
+++ keyboard.py
@@ -1,12 +1,8 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-WEATHER_BTN = InlineKeyboardButton(text="Weather", callback_data="weather")
-SUN_BTN = InlineKeyboardButton(text="sunrise and sunset", callback_data="sun")
-
 CITY_MODE = InlineKeyboardButton(text="Your city name", callback_data="city")
 LOCATION_MODE = InlineKeyboardButton(text="Your location", callback_data="location")
 
-Inline = InlineKeyboardMarkup(row_width=2).add(WEATHER_BTN, SUN_BTN)
 Markup = InlineKeyboardMarkup(row_width=2).add(CITY_MODE, LOCATION_MODE)
 
